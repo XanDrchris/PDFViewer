4ddf741fa32a6b77686ef5a52575423810546dba
Xander Christopher <93678143+XandrChris@users.noreply.github.com>
2023-09-24 00:46:02 +0530

    update

--- sw.js
+++ sw.js
@@ -1,10 +1,21 @@
 self.addEventListener('install', event => {
-
+    console.log("Service Worker install Event");
+    event.waitUntil(
+        caches.open(cacheName)
+        .then(function(cache){
+            return cache.addAll(resourcesToPreCache);
+        })
+    )
 })
 const cacheName = 'PDFViewer-v1.0.0';
 const resourcesToPreCache = [
     '/PDFViewer/',
     '/PDFViewer/index.html',
     '/PDFViewer/Styles/mycss.css',
-    '/PDFViewer/Styles/viewer.css'
-]
+    '/PDFViewer/Styles/viewer.css',
+];
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request) || fetch(event.request)
+    )
+})
